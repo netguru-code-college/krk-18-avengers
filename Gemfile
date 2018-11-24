15e9d3aae038b6524feaece6dd42dea43b405838
Gabi Fijalkowska <gabrielafijalkowska@gmail.com>
2018-11-24 13:38:15 +0100

    Add Nasa Service to fetch image url (#3)
    * Add Nasa Service to fetch image url
    
    * Add fetching a random image

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bootstrap'
 gem 'jquery-rails'
 gem 'devise'
+gem 'httparty'
 
 group :development, :test do
   gem 'pry-rails'
---
+++ app/services/nasa_service.rb
@@ -0,0 +1,11 @@
+class NasaService
+  include HTTParty
+  base_uri "https://api.nasa.gov"
+
+  def get_image_url(action_name='planetary')
+    count = (10..100).to_a.sample
+    results = self.class.get("/#{action_name}/apod?api_key=#{Rails.application.credentials.nasa_apikey}&count=#{count}")
+    result = results.sample
+    result.dig('url')
+  end
+end
