b492fb762ed443521cd75de3712cb10d15abeabe
Marcin Krolik <krolikmarcin.mk@gmail.com>
2018-11-24 16:29:59 +0100

    Fix changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,5 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
-  before_action :fetch_event_details, only: [:new, :create]
 
   def index
     @events = current_user.events
@@ -14,14 +13,16 @@ class EventsController < ApplicationController
 
   def new
     @event = Event.new
+    @users = User.all
   end
 
   def create
-    @event = current_user.owned_events.build(event_params.merge(image_url: @image))
-    pry binding
+    image_url = NasaService.new.get_image_url
+    @event = current_user.owned_events.build(event_params.merge(image_url: image_url))
     if @event.save
       redirect_to event_path(@event)
     else
+      @users = User.all
       render :new
     end
   end
@@ -29,10 +30,5 @@ class EventsController < ApplicationController
   private
   def event_params
     params.require(:event).permit(:name, :date)
-  end
-
-  def fetch_event_details
-    @image = NasaService.new.get_image_url
-    @users = User.all
   end
 end
