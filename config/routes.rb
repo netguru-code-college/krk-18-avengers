767cdfc8ca3fd182068fbaf0f399cc6f425c2d5b
Tomasz Jaśkiewicz <tomasz.jaskiewicz@netguru.com>
2018-11-24 16:12:00 +0100

    Remove post resource (#6)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
 
   root 'main_screen#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
