169353d9a5da03daa6c3cd4bdee24f317fc1ffe8
Dobeb96 <dobosz.ma@interia.pl>
2018-11-25 13:39:24 +0100

    Calculate debt amounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ApplicationRecord
   has_many :users_products
   has_many :products, through: :users_products
   has_many :owned_events, class_name: 'Event', foreign_key: :owner_id
+
+  def others_debt_count
+    sum = 0
+    owned_events.each do |event|
+      sum += event.users_products.where(is_paid: false).sum(&:amount)
+    end
+    sum
+  end
+
+  def users_debt_count
+    users_products.where(is_paid: false).sum(:amount)
+  end
 end
