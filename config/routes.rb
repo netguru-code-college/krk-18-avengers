6c62f1eaaecef345e0628ce590049773e86f656a
Marcin Krolik <krolikmarcin.mk@gmail.com>
2018-11-25 14:20:23 +0100

    Create products_controller #pay_bill with route and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events, only: [:index, :show, :create, :new] do
-    resources :products, only: [:show, :new, :create]
+    resources :products, only: [:show, :new, :create] do
+      get 'pay_bill'
+    end
   end
   root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
